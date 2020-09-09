2642cd5d4ceb634fcb7b3a3458cc2c3d28f11672
Will Page <billyoyo@hotmail.co.uk>
2020-09-09 10:16:46 +0100

    finished part 2

--- app/src/main/java/com/example/fizzbuzz/main.kt
+++ app/src/main/java/com/example/fizzbuzz/main.kt
@@ -1,5 +1,38 @@
 package com.example.fizzbuzz;
 
 fun main() {
-    println("hello world");
+    for (i in 1..100) {
+        val s = ArrayList<String>();
+        val isBong = i % 11 == 0;
+
+        if(!isBong && i % 3 == 0) {
+            s.add("Fizz");
+        }
+
+        if(i % 13 == 0) {
+            s.add("Fezz");
+        }
+
+        if (!isBong && i % 5 == 0) {
+            s.add("Buzz");
+        }
+
+        if (!isBong && i % 7 == 0) {
+            s.add("Bang");
+        }
+
+        if (isBong) {
+            s.add("Bong");
+        }
+
+        if (i % 17 == 0) {
+            s.reverse();
+        }
+
+        if (s.isEmpty()) {
+            println(i);
+        } else {
+            println(s.joinToString(separator = ""));
+        }
+    }
 }
